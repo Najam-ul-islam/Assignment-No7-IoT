73f56d906f989f32d0bd03995012f77046f6c260
Muhammad Najam Ul Islam <26120402+Najam-ul-islam@users.noreply.github.com>
2020-01-15 16:04:30 +0500

    3rd commit

--- question-2/src/lib.rs
+++ question-2/src/lib.rs
@@ -1,10 +1,10 @@
-pub mod truth_table{
+mod truth_table{
 	pub mod xor{
 		pub fn simple_function(){
 			let input = [[0,0],
-					 	 [0,1],
-					 	 [1,0],
-					 	 [1,1]];
+					 	[0,1],
+					 	[1,0],
+					 	[1,1]];
 
 			let output = [0,1,1,0];
 			println!("============");
@@ -14,7 +14,7 @@ pub mod truth_table{
 			println!(" {:?}  {:?} \t  {:?}",input[1][0],input[1][1],output[1]);
 			println!(" {:?}  {:?} \t  {:?}",input[2][0],input[2][1],output[1]);
 			println!(" {:?}  {:?} \t  {:?}",input[3][0],input[3][1],output[0]);
-            println!("============");
+
 		} 
 	} 
 }
--- question-3/src/main.rs
+++ question-3/src/main.rs
@@ -1,3 +1,19 @@
+/* Q3. Write a Rust library,
+● Make a library package
+● Make a module with suitable name in library
+● Make a sub module with an other name in first module
+● Define a simple function in sub module
+● make a binary package
+● add your library in dependencies in cargo.toml
+● use your library in main.rs
+● Call that function from fn main
+*/
+use question_3_lib;
+
 fn main() {
-    println!("Hello, world!");
+    let mut lst:[u8;12] = [2,1,10,4,3,5,7,6,9,8,0,15];
+    println!("Original Array:{:?}",lst);
+    // Function call
+     let bubble = question_3_lib::algorithm::sorting::bubble_sort(lst);
+    println!("New Array: {:?}",bubble);
 }
